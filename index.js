bdd63166d6e349ed3a427b1dae85d5c9c0984476
eliasadmaso1 <eliasadmas01@gmail.com>
2022-05-04 11:41:11 +0300

    commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const database = require('./db');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
 
 
 const app = express();
@@ -12,9 +14,34 @@ const PORT = process.env.PORT || 6000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
 const userRouter = require('./Routes/userRoutes');
 const postRouter = require('./Routes/postRoutes');
 const authRouter = require('./Routes/authRoutes');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({storage});
+  
+  app.post('/upload',upload.single('file'),(req,res)=>{
+    try{
+      return res.status(200).json("File uploaded successfully")
+    }
+    catch(error){
+      console.log(error);
+  
+    }
+  
+  })
 
 
 app.use(express.json());
